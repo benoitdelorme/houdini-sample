2fbde3ce9e779b7a14bc9aefddccdb4a65d9815a
Benoît <delorme.benoit@gmail.com>
2020-04-18 21:59:43 -0400

    properties and values gradient-transition

---
+++ properties-and-values/gradient-transition/assets/js/index.js
@@ -0,0 +1,13 @@
+CSS.registerProperty({ 
+    name: "--colorPrimary",
+    syntax: "<color>", 
+    inherits: false,
+    initialValue: "rgba(9,9,121,1)"
+});
+
+CSS.registerProperty({ 
+    name: "--colorSecondary",
+    syntax: "<color>", 
+    inherits: false,
+    initialValue: "rgba(0,212,255,1)"
+});
